6941f205f0a9b19c6a3861b83d8853de48ad39fc
Ahammad Reyad <ahammad.reyad@gmail.com>
2016-04-11 19:57:25 +0600

    58748_lesson

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,6 +2,7 @@ class Word < ActiveRecord::Base
   belongs_to :category
   has_many :results
   has_many :answers, dependent: :destroy
+  has_many :lessons, through: :results
   accepts_nested_attributes_for :answers,
     reject_if: lambda {|attribute| attribute[:content].blank?},
     allow_destroy: true
