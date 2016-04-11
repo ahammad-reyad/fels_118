2406bcb527928ca6740b44c5a6902d7072ca6d7c
Ahammad Reyad <ahammad.reyad@gmail.com>
2016-04-11 10:19:14 +0600

    58744_category_list

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   post   "login"  => "sessions#create"
   delete "logout" => "sessions#destroy"
   resources :users
+  resources :categories
+  resources :lessons
   namespace :admin do
     root "users#index"
     resources :users
