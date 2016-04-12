aa531a017056a8d47c7e1081a28378e08d2fba3d
Ahammad Reyad <ahammad.reyad@gmail.com>
2016-04-12 11:38:09 +0600

    58751_activities

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,7 @@
 class RelationshipsController < ApplicationController
   before_action :logged_in_user
+  before_action :create_follow_activity, only: [:create]
+  before_action :create_unfollow_activity, only: [:destroy]
 
   def index
     user = User.find params[:user]
@@ -17,5 +19,20 @@ class RelationshipsController < ApplicationController
     user = Relationship.find(params[:id]).followed
     current_user.unfollow user
     redirect_to user
+  end
+
+  private
+  def create_follow_activity
+    user = User.find params[:followed_id]
+    Activity.create user_id: current_user.id, details:
+      I18n.t("started_following", follower_name: current_user.name,
+      name: user.name)
+  end
+
+  def create_unfollow_activity
+    user = Relationship.find(params[:id]).followed
+    Activity.create user_id: current_user.id, details:
+      I18n.t("started_unfollowing", unfollower_name: current_user.name,
+      name: user.name)
   end
 end
